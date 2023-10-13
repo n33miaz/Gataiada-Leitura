e7ccfa4638c5103dba6a199f37ec78922371377d
n33miaz <ncormino@gmail.com>
2023-10-13 17:27:56 -0300

    add: Menu de Navegação em todos os index.html feat: Redirecionamentos do Menu de Navegação

---
+++ src/pages/recomendacoes/js/header.js
@@ -0,0 +1,15 @@
+document.getElementById("principal").addEventListener("click", function() {
+    window.location.href = "/index.html";
+});
+
+document.getElementById("recomendacoes").addEventListener("click", function() {
+    window.location.href = "./src/pages/recomendacoes/index.html";
+});
+
+document.getElementById("literatura").addEventListener("click", function() {
+    window.location.href = "/src/pages/literatura/index.html";
+});
+
+document.getElementById("gatos").addEventListener("click", function() {
+    window.location.href = "/src/pages/gatos/index.html";
+});
