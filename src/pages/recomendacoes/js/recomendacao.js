85360581a8be349d279fd8a427e1e74810ea342e
n33miaz <ncormino@gmail.com>
2023-10-14 14:15:05 -0300

    feat: Painel de Recomendação de Livro no index.html (/recomendacao) e add: recomendacao.css & recomendacao.js

---
+++ src/pages/recomendacoes/js/recomendacao.js
@@ -0,0 +1,53 @@
+const formulario = document.querySelector(".formulario-de-recomendacao");
+
+document.getElementById("botao-abrir-recomendacao").addEventListener("click", function() {
+    event.preventDefault();
+    formulario.classList.add("formulario-recomendacao-aberto");
+});
+
+document.getElementById("botao-fechar-recomendacao").addEventListener("click", function() {
+    event.preventDefault();
+    formulario.classList.remove("formulario-recomendacao-aberto");
+});
+
+function mostrarPopup() {
+    const popup = document.createElement("div");
+    popup.className = "popup";
+    popup.innerHTML = "Recomendação Enviada com Sucesso!";
+    popup.style.position = "fixed";
+    popup.style.top = "50%";
+    popup.style.left = "50%";
+    popup.style.transform = "translate(-50%, -50%)";
+    popup.style.backgroundColor = "#fff";
+    popup.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.3)";
+    popup.style.padding = "20px";
+    popup.style.zIndex = "5";
+    const popupBackground = document.createElement("div");
+    popupBackground.className = "popup-background";
+    popupBackground.style.position = "fixed";
+    popupBackground.style.top = "0";
+    popupBackground.style.left = "0";
+    popupBackground.style.width = "100%";
+    popupBackground.style.height = "100%";
+    popupBackground.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    popupBackground.style.zIndex = "2";
+    document.body.appendChild(popupBackground);
+    document.body.appendChild(popup);
+};
+
+document.getElementById("botao-enviar-recomendacao").addEventListener("click", function(event) {
+    const nomeInput = document.querySelector(".nome-recomendacao");
+    const emailInput = document.querySelector(".email-recomendacao");
+    const tituloInput = document.querySelector(".titulo-recomendacao");
+    const descricaoInput = document.querySelector(".descricao-recomendacao");
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (
+        nomeInput.value.trim() !== "" &&
+        emailRegex.test(emailInput.value.trim()) &&
+        tituloInput.value.trim() !== "" &&
+        descricaoInput.value.trim() !== ""
+    ) {
+        mostrarPopup();
+    }
+});
